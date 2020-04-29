7a385adcdec71d2d4688a5f303628bad2d9b0a47
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-04-29 09:48:17 +0100

    Add models

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema  = mongoose.Schema;
+
+// Create Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: new Date.now()
+  },
+});
+
+const User = mongoose.model('users', userSchema);
+module.exports = User;
