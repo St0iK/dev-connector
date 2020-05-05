e9e97dd7b2650a2ee0656b310732cccafca7e9c8
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-05-05 08:03:56 +0100

    Added frontend

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <p className="lead">
+                  {' '}
+                  Create a developer profile/portfolio, share posts and get help
+                  from other developers
+                </p>
+                <hr />
+                <Link className="btn btn-lg btn-info mr-2" to="/register">
+                  Sign Up
+                </Link>
+                <Link className="btn btn-lg btn-light" to="/login">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
