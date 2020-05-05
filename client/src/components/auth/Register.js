e3ecb52b8ee473c3c53eabe05238d7aa14f8ceb6
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-05-05 08:33:03 +0100

    onChange

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -11,6 +11,18 @@ class Register extends Component {
             errors: '',
         }
     }
+
+    /**
+     * On Change
+     * @param {*} e 
+     */
+    onChange(e) {
+        const elementName = e.target.name;
+        const elementValue = e.target.value;
+        console.log('test');
+        this.setState({ [elementName]: elementValue })
+    }
+
     render() {
         return (
             <div className="register">
@@ -23,18 +35,27 @@ class Register extends Component {
                                 <div className="form-group">
                                     <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" required
                                         value={this.state.name}
-                                        onChange={onchange}
+                                        onChange={this.onChange.bind(this)}
                                     />
                                 </div>
                                 <div className="form-group">
-                                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} />
+                                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"
+                                        value={this.state.email}
+                                        onChange={this.onChange.bind(this)}
+                                    />
                                     <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} />
+                                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"
+                                        value={this.state.password}
+                                        onChange={this.onChange.bind(this)}
+                                    />
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} />
+                                    <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2"
+                                        value={this.state.password2}
+                                        onChange={this.onChange.bind(this)}
+                                    />
                                 </div>
                                 <input type="submit" className="btn btn-info btn-block mt-4" />
                             </form>
